b87385e115c09e8855b36853f4c69426cf5e8e19
sxgxhk <jslfp@foxmail.com>
2023-04-21 13:46:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Feng's Blog',
   author: 'Feng',
   email: 'jslfp@foxmail.com',
-  link: 'https://blog.typecho.win',
+  link: 'https://blog.1984n.win',
   description: 'This gonna be an awesome website.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
