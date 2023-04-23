147d6e72cea74827612574fbca627dd963cd9a82
sxgxhk <jslfp@foxmail.com>
2023-04-23 12:34:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: 'https://twikoo-five.vercel.app', // leave it empty if you don't need any comment plugin
+    provider: '', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: 'ca0ba3b1-84a4-4973-8520-7cd7d65ca830', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
